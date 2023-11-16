d836a79b1b1b2e0b02eea345e06c77d3a77aa930
laiskasiili <marc.folini@gmail.com>
2023-11-16 22:53:36 +0000

    Rewrite landing solareignung script to use the storage backend abstraction and general improvements to readability

--- etl_scripts/landing_solareignung.py
+++ etl_scripts/landing_solareignung.py
@@ -11,78 +11,86 @@
 # - Fetch the STAC metadata from the source url and extract updated timestamp and asset download url.
 # - Check the cloud storage for an existing asset. If it is outdated or non-existent, download the asset and save it in the landing zone.
 
-from utils_misc import fetch
-from utils_gcs import list_blobs, delete_blob, write_blob
+from utils import GoogleCloudStorageIoManager, LocalIoManager, fetch
 from datetime import datetime
 
+# LANDING_FOLDER = LocalIoManager("./landing/ch.bfe.solarenergie-eignung-daecher/")
+LANDING_FOLDER = GoogleCloudStorageIoManager(
+    bucket_name="folimar-geotest-store001",
+    prefix="landing/ch.bfe.solarenergie-eignung-daecher/",
+)
 SOURCE_STAC_URL = "https://data.geo.admin.ch/api/stac/v0.9/collections/ch.bfe.solarenergie-eignung-daecher/items/solarenergie-eignung-daecher"
-SINK_LOCATION = "folimar-geotest-store001/landing/ch.bfe.solarenergie-eignung-daecher"
 STAC_ASSET_KEYS = ["solarenergie-eignung-daecher_2056.gdb.zip"]
 
 
-def get_expected_asset_blob_names(stac_item_url: str) -> (set, dict):
+def get_expected_asset_names() -> (set, dict):
     """
-    Parse the stac file and create a set of expected asset blob names.
-    The asset blob names are constructed from the last part of the asset url,
-    prefixed by the updated timestamp in format YYYYMMDD and the sink location.
+    Parse the stac file and create a set of expected asset paths.
+    The expected asset name is constructed from the last part of the asset url,
+    prefixed by the updated timestamp in format YYYYMMDD.
     Example:
     1) Stac asset json: {"updated": "2023-11-16T00:00:00.000Z", "href": "https://data.geo.admin.ch/ch.bfe.solarenergie-eignung-daecher/2056/gdb/ch.bfe.solarenergie-eignung-daecher_2056.gdb.zip"}
-    2) Sink location: folimar-geotest-store001/landing/ch.bfe.solarenergie-eignung-daecher
-    3) Resulting full blob name: folimar-geotest-store001/landing/ch.bfe.solarenergie-eignung-daecher/20231116_solarenergie-eignung-daecher_2056.gdb.zip
+    3) Resulting asset name: 20231116_solarenergie-eignung-daecher_2056.gdb.zip
     """
-    stac_item = fetch(stac_item_url).json()
-    blob_names = set()
-    blob_names_url_map = {}
+    stac_item = fetch(SOURCE_STAC_URL).json()
+    asset_names = set()
+    asset_names_url_map = {}
     for key in STAC_ASSET_KEYS:
-        asset_info = stac_item["assets"][key]
-        url_part = asset_info["href"].split("/")[-1]
+        asset = stac_item["assets"][key]
+        url_part = asset["href"].split("/")[-1]
         date_part = datetime.strptime(
-            asset_info["updated"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            asset["updated"], "%Y-%m-%dT%H:%M:%S.%fZ"
         ).strftime("%Y%m%d")
-        blob_name = f"{SINK_LOCATION}/{date_part}_{url_part}"
-        blob_names.add(blob_name)
-        blob_names_url_map[blob_name] = asset_info["href"]
-    return blob_names, blob_names_url_map
+        asset_name = f"{date_part}_{url_part}"
+        asset_names.add(asset_name)
+        asset_names_url_map[asset_name] = asset["href"]
+    return asset_names, asset_names_url_map
 
 
-def get_existing_asset_blob_names(sink_location: str) -> set:
+def get_existing_asset_asset_names() -> set:
     """Retrieve the list of currently stored zip files."""
-    return set(b for b in list_blobs(sink_location))
+    return set(LANDING_FOLDER.list())
 
 
-def remove_outdated_assets(existing_assets: set, expected_assets: set) -> None:
+def remove_outdated_assets(
+    existing_asset_names: set, expected_asset_names: set
+) -> None:
     """Identify and remove outdated or deleted assets."""
-    to_delete = existing_assets - expected_assets
+    to_delete = existing_asset_names - expected_asset_names
     total_to_delete = len(to_delete)
-    for i, blob_name in enumerate(to_delete, 1):
-        print(f"{i}/{total_to_delete} Delete: {blob_name}")
-        delete_blob(blob_name)
+    for i, asset_name in enumerate(to_delete, 1):
+        print(
+            f"{i}/{total_to_delete} Delete: {LANDING_FOLDER.absolute_path(asset_name)}"
+        )
+        LANDING_FOLDER.delete(asset_name)
 
 
-def fetch_and_upload_assets(
-    expected_assets: set, existing_assets: set, asset_url_map: dict
+def fetch_and_save_assets(
+    expected_asset_names: set, existing_asset_names: set, asset_url_map: dict
 ) -> None:
     """Fetch and store the new or updated assets."""
-    to_fetch = expected_assets - existing_assets
+    to_fetch = expected_asset_names - existing_asset_names
     total_to_fetch = len(to_fetch)
-    for i, blob_name in enumerate(to_fetch, 1):
-        print(f"{i}/{total_to_fetch} Fetch and upload: {blob_name}")
-        url = asset_url_map[blob_name]
-        write_blob(blob_name, fetch(url).content, "wb")
+    for i, asset_name in enumerate(to_fetch, 1):
+        print(
+            f"{i}/{total_to_fetch} Fetch and save: {LANDING_FOLDER.absolute_path(asset_name)}"
+        )
+        url = asset_url_map[asset_name]
+        LANDING_FOLDER.write(asset_name, fetch(url).content, "wb")
 
 
 def main():
     # Extract expected asset locations from stac metadata
-    expected_assets, asset_url_map = get_expected_asset_blob_names(SOURCE_STAC_URL)
+    expected_asset_names, asset_url_map = get_expected_asset_names()
 
     # Retrieve the list of currently stored assets
-    existing_assets = get_existing_asset_blob_names(SINK_LOCATION)
+    existing_asset_names = get_existing_asset_asset_names()
 
     # Identify and remove outdated or deleted assets
-    remove_outdated_assets(existing_assets, expected_assets)
+    remove_outdated_assets(existing_asset_names, expected_asset_names)
 
     # Fetch and store the new or updated assets
-    fetch_and_upload_assets(expected_assets, existing_assets, asset_url_map)
+    fetch_and_save_assets(expected_asset_names, existing_asset_names, asset_url_map)
 
     print("All done!")
 
